10145d3cc714d96ea9238c6fc281b16525375f28
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2022-11-08 23:18:13 -0600

    add likes and comments counts

--- tweets/models.py
+++ tweets/models.py
@@ -1,19 +1,30 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models.signals import post_save
 from likes.models import Like
 from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
-from tweets.listeners import push_tweet_to_cache
+from utils.time_helpers import utc_now
+from django.db.models.signals import post_save
 from utils.listeners import invalidate_object_cache
 from utils.memcached_helper import MemcachedHelper
-from utils.time_helpers import utc_now
+from tweets.listeners import push_tweet_to_cache
 
 
 class Tweet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text='who create this tweet'
+    )
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # 新增的 field 一定要设置 null=True，否则 default = 0 会遍历整个表单去设置
+    # 导致 Migration 过程非常慢，从而把整张表单锁死，从而正常用户无法创建新的 tweets
+    likes_count = models.IntegerField(default=0, null=True)
+    comments_count = models.IntegerField(default=0, null=True)
+
 
     class Meta:
         index_together = (('user', 'created_at'),)
@@ -78,6 +89,5 @@ class TweetPhoto(models.Model):
     def __str__(self):
         return f'{self.tweet_id}: {self.file}'
 
-
-post_save.connect(invalidate_object_cache, sender=Tweet)
-post_save.connect(push_tweet_to_cache, sender=Tweet)
+    post_save.connect(invalidate_object_cache, sender=Tweet)
+    post_save.connect(push_tweet_to_cache, sender=Tweet)
